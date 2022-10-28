66ac0fcdea3a12998bfecf93e411f5a835f2ccd1
Jaewook Kim <jaewkim@c3r6s1.42seoul.kr>
2022-10-28 17:55:38 +0900

    first react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState(0);
+
+  const increaseNumber = () => {
+    setState(state + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>값: {state}</div>
+      <button onClick={() => increaseNumber()}>증가하기</button>
     </div>
   );
 }
