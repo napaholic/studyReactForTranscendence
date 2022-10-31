d753f11206add0da028f3678a3cdb92c43944e52
Jaewook Kim <jaewkim@c3r4s3.42seoul.kr>
2022-10-31 17:02:17 +0900

    del modules

--- my-first-react/src/index.tsx
+++ my-first-react/src/index.tsx
@@ -11,6 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <BrowserRouter>
+
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} />
