b6b9e659c2aba4f27fdbabddc273171247767539
napaholic <jaewkim@student.42seoul.kr>
2022-11-01 18:12:45 +0900

    node connection complete

--- inflearn-react/index.js
+++ inflearn-react/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jaewkim:abcd1234@first-react.fw5iflv.mongodb.net/?retryWrites=true&w=majority', {
+}).then(() => console.log('MongoDB Connected...'))
+	.catch(err => console.log(err))
+
+
+
 app.get('/', (req, res) => {
 	res.send('Hello World! 안녕하세요ㄴㅇㄹㄴㅇㄹ')
 })
